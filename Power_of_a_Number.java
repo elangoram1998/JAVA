a3aa993d1b8c273a7beeb3a9704d91b4ff6c5960
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-05 21:53:24 +0530

    Create Power_of_a_Number.java

---
+++ Power_of_a_Number.java
@@ -0,0 +1,14 @@
+public class Power {
+
+	public static void main(String[] args) {
+	int a=50;
+	int b=2;
+	int result=1;
+	for(int i=0;i<2;i++)
+	{
+		result=result*a;
+	}
+System.out.println(result);
+	}
+
+}
