9c4ff05ee6d57933d298a4bc3dc0999f0d18bfc2
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-19 21:03:53 +0530

    Create Count_Vowels.java

---
+++ Assignments/Count_Vowels.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Vowel_count {
+
+	public static void main(String[] args) {
+		String str;
+		int i,j,count=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string:");
+		str=sc.nextLine();
+		str=str.toLowerCase();
+		char[] array=str.toCharArray();
+		for(i=0;i<str.length()-1;i++)
+		{
+			for(j=i+1;j<str.length();j++)
+			{
+				if(array[i]==array[j])
+				{
+					array[i]='\n';
+				}
+			}
+		}
+		for(i=0;i<str.length();i++)
+		{
+			if(array[i]=='a'||array[i]=='e'||array[i]=='i'||array[i]=='o'||array[i]=='u')
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
