baf30cdd6643498b4258a92bcc409b975d0a1189
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-02-22 21:44:06 +0530

    Create Java_Substring_Comparisons.java

---
+++ Hackerrank/Java_Substring_Comparisons.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = s.substring(0,k);
+        int a=k-1;
+        int i;
+		for(i=1;i<s.length()-a;i++)
+		{
+			k++;
+			smallest=s.substring(i, k);
+			
+		}
+        
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
