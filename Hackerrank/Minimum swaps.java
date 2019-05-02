6f2aa8a6ba4c8216ff596da488b80fcef69c6260
Elango R <30955305+elangoram1998@users.noreply.github.com>
2019-05-02 09:02:15 +0530

    Create Minimum swaps.java

---
+++ Hackerrank/Minimum swaps.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int n=arr.length;
+        int temp=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i+1!=arr[i])
+            {
+                int t=i;
+                while(arr[t]!=i+1)
+                {
+                    t++;
+                }
+                int num=arr[i];
+                arr[i]=arr[t];
+                arr[t]=num;
+                temp++;
+            }
+        }
+        return temp;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
